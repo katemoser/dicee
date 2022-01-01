63baee74224deb7b4f7d6a48455c7a2adbda3881
Kate Moser <komoser@gmail.com>
2022-01-01 09:29:16 -0500

    Finished

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-
-
-console.log("Hello")
+let players = [];
 
 function generateRandomNumber(){
     return Math.floor(Math.random() *6) + 1;
 }
 
-function rollDie(element){
-    element.setAttribute("img", "images/dice1.png")
+function rollDie(element, num){
+    element.setAttribute("src", "images/dice" + num + ".png")
 }
 
-randomNumber1 = generateRandomNumber();
-console.log(randomNumber1);
+function assignWinner(){
+    //get the header so i can change it
+    let result = document.querySelector("h1");
+    if(players[0] > players[1]){
+        result.innerHTML = "Player 1 Wins!";
+        console.log("Player 1 Wins");
+    }
+    else if(players[1] > players[0]){
+        result.innerHTML = "Player 2 Wins!";
 
+        console.log("Player 2 Wins");   
+    }
+    else{
+        result.innerHTML = "Draw!";
+
+        console.log("Draw");
+    }
+}
 
 //select the two dice, and roll them
-let dice = document.getElementsByClassName("dice");
-console.log(dice);
+let dice = document.querySelectorAll("img");
+//roll dice and save result
 for(let die of dice){
-    console.log("working");
-    rollDie(die);
+    let num = generateRandomNumber();
+    players.push(num);
+    rollDie(die, num);
 }
+assignWinner();
